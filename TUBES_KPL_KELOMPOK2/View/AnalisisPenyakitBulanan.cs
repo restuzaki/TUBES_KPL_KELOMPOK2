1f7a71f0ccecdf8132a1f54cc8b8bda3f2f72df4
Reiluthfi <164453246+Reiluthfi@users.noreply.github.com>
2025-05-04 21:23:11 +0700

    Fitur Analisis dan Beli obat

---
+++ TUBES_KPL_KELOMPOK2/View/AnalisisPenyakitBulanan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Apotekku_API.Models;
+public class AnalisisPenyakit
+{
+    private List<PenyakitAnalisis> diseaseAnalyses;
+    public AnalisisPenyakit()
+    {
+        var configJson = File.ReadAllText("Data\\PenyakitAnalisis.Json");
+        diseaseAnalyses = JsonSerializer.Deserialize<List<PenyakitAnalisis>>(configJson);
+    }
+
+    public IEnumerable<PenyakitAnalisis> GetDiseaseAnalyses()
+    {
+        return diseaseAnalyses; 
+    }
+    public async Task TampilkanAnalisisAsync()
+    {
+        Console.WriteLine("\n--- Analisis Penyakit Bulanan ---");
+
+        foreach (var penyakit in diseaseAnalyses)
+        {
+            Console.WriteLine($"Nama: {penyakit.NamaPenyakit}, Jumlah Kasus: {penyakit.JumlahKasus}");
+        }
+
+        await Task.CompletedTask; // agar tetap async-compatible
+    }
+
+}
