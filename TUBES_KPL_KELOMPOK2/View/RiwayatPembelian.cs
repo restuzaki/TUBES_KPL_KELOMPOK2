94754a14bcf7e52a268f7f8dc1408dca66d4525f
Bagus Hardiyanto <bagushardiyanto03@gmail.com>
2025-05-04 22:18:27 +0700

    manambahkan riwayat pembelian

---
+++ TUBES_KPL_KELOMPOK2/View/RiwayatPembelian.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apotekku_API.Models;
+
+namespace TUBES_KPL_KELOMPOK2.View
+{
+    public class RiwayatPembelian
+    {
+        public DateTime Tanggal { get; set; }
+        public List<Obat> Obats { get; set; }
+
+        public RiwayatPembelian(DateTime tanggal, List<Obat> obats)
+        {
+            Tanggal = tanggal;
+            Obats = obats;
+        }
+    }
+
+}
