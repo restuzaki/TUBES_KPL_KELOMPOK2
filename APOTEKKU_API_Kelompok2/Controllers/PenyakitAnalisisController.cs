101f69e61e4edc19fb294c01501215526e10ae05
fathanPrayoga <fathan.wicaksono@gmail.com>
2025-05-04 07:45:50 +0700

    menambahkan API

---
+++ APOTEKKU_API_Kelompok2/Controllers/PenyakitAnalisisController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Apotekku_API.Models;
+
+namespace Apotekku_API.Controllers
+{
+        [ApiController]
+        [Route("api/[controller]")]
+
+        public class PenyakitAnalisisController : Controller
+        {
+            private static string jsonFilePath = "Data/PenyakitAnalisis.json";
+
+        [HttpGet]
+            public ActionResult<List<PenyakitAnalisis>> Get()
+            {
+                string jsonString = System.IO.File.ReadAllText("Data/PenyakitAnalisis.json");
+                var result = JsonSerializer.Deserialize<List<PenyakitAnalisis>>(jsonString);
+                if (result == null)
+                {
+                    result = new List<PenyakitAnalisis>();
+                }
+                return Ok(result);
+            }
+            
+
+        }
+    }
