256bff00829dd7be832b14102f4cd822196c07b1
Reiluthfi <164453246+Reiluthfi@users.noreply.github.com>
2025-06-01 11:18:15 +0700

    GUI AnalisisPenyakit Dan Pembelian Obat

---
+++ GUI_KPL/PembelianObatForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apotekku_API.Models;
+using TUBES_KPL_KELOMPOK2.View;
+
+namespace GUI_KPL
+{
+    public partial class PembelianObatForm: Form
+    {
+        private PembelianObat _pembelian;
+        private string _currentUser;
+        public PembelianObatForm(string currentUser)
+        {
+            InitializeComponent();
+            _pembelian = new PembelianObat();
+            LoadObatDropdown();
+            LoadPembelianGrid();
+            _currentUser = currentUser;
+        }
+        private void LoadObatDropdown()
+        {
+            var obats = _pembelian.GetObatsFromAPI();
+            comboBox1.Items.Clear();
+            foreach (var obat in obats)
+            {
+                comboBox1.Items.Add(obat.nama);
+            }
+        }
+        private void LoadPembelianGrid()
+        {
+            var pembelianData = _pembelian.GetObats()
+                .Select(o => new
+                {
+                    Nama = o.nama,
+                    Harga = o.harga,
+                    Jumlah = o.stok,
+                    Total = o.harga * o.stok
+                }).ToList();
+
+            dataGridView1.DataSource = pembelianData;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string namaObat = comboBox1.SelectedItem?.ToString();
+            if (!int.TryParse(textBox1.Text, out int jumlah) || jumlah <= 0)
+            {
+                MessageBox.Show("Masukkan jumlah obat yang valid.");
+                return;
+            }
+
+            int harga = _pembelian.GetHargaObat(namaObat);
+            try
+            {
+                _pembelian.TambahObat(namaObat, jumlah, harga, DateTime.Now);
+                RefreshGrid();
+                MessageBox.Show("Obat ditambahkan ke keranjang.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            SimpanRiwayat(_pembelian.GetObats().ToList());
+         
+            MessageBox.Show("Obat Berhasil Dibeli!", "Pembelian Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear(); 
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            User currentUser = new User
+            {
+                Nama = _currentUser,
+                Role = "Buyer"
+            };
+            MenuUser menuUser = new MenuUser(currentUser);
+            menuUser.Show();
+            this.Hide();
+
+        }
+        private void RefreshGrid()
+        {
+            var data = _pembelian.GetObats().Select(o => new
+            {
+                Nama = o.nama,
+                Harga = o.harga,
+                Jumlah = o.stok,
+                Total = o.harga * o.stok
+            }).ToList();
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = data;
+        }
+        private void SimpanRiwayat(List<Obat> daftarPembelian)
+        {
+            string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+            string filePath = Path.Combine(folderPath, "riwayat.json");
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            string json = JsonSerializer.Serialize(daftarPembelian, options);
+            File.WriteAllText(filePath, json);
+        }
+
+    }
+}
--- GUI_KPL/obj/Debug/net8.0-windows/GUI_KPL.AssemblyInfo.cs
+++ GUI_KPL/obj/Debug/net8.0-windows/GUI_KPL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI_KPL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64747ac0bcc0801a0bd8b72ff137705080e3d4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c990837a56e7222cc2c8397d2f98611038df4e9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI_KPL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI_KPL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
